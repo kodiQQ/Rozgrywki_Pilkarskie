76de5a55ac41d38e94dc1c90d83a737d4e9ffbc0
Konrad Kunicki <konrad.kunicki2003@wp.pl>
2024-04-13 17:49:02 +0200

    Pododawałem modele do panelu admina

--- mysite/base/admin.py
+++ mysite/base/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Match, Team, Statistics, Season_Table, Participation, Player, Contract, Squad, Scorer_Table
+
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(Statistics)
+admin.site.register(Season_Table)
+admin.site.register(Participation)
+admin.site.register(Player)
+admin.site.register(Contract)
+admin.site.register(Squad)
+admin.site.register(Scorer_Table)
--- mysite/base/models.py
+++ mysite/base/models.py
@@ -12,9 +12,27 @@ class Team(models.Model):
     city=models.CharField(max_length=50)
     def __str__(self):
         return self.name
+class Statistics(models.Model):
+    team=models.ForeignKey(Team,on_delete=models.CASCADE)
+    wins=models.IntegerField()
+    draws=models.IntegerField()
+    loses=models.IntegerField()
+    goals=models.IntegerField()
+    goals_lost=models.IntegerField()
+    points=models.IntegerField()
+
+    def calculate_points(self):
+        return self.wins * 3 + self.draws  # Punkty za zwycięstwo: 3, za remis: 1, za porażkę: 0
+class Season_Table(models.Model):
+    team=models.ForeignKey(Team,on_delete=models.CASCADE)
+    statistics= models.ForeignKey(Statistics, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['statistics__points']
+    def __str__(self):
+        return self.name
 class Participation(models.Model):
-    team=models.ForeignKey(Team,on_delete=models.CASCADE())
-    match=models.ForeignKey(Match,on_delete=models.CASCADE())
+    team=models.ForeignKey(Team,on_delete=models.CASCADE)
+    match=models.ForeignKey(Match,on_delete=models.CASCADE)
 
 class Player(models.Model):
     name = models.TextField(max_length=20)
@@ -29,24 +47,7 @@ class Player(models.Model):
 
 
 
-class Statistics(models.Model):
-    team=models.ForeignKey(Team,on_delete=models.CASCADE())
-    wins=models.IntegerField()
-    draws=models.IntegerField()
-    loses=models.IntegerField()
-    goals=models.IntegerField()
-    goals_lost=models.IntegerField()
-    points=models.IntegerField()
 
-    def calculate_points(self):
-        return self.wins * 3 + self.draws  # Punkty za zwycięstwo: 3, za remis: 1, za porażkę: 0
-class Season_Table(models.Model):
-    team=models.ForeignKey(Team,on_delete=models.CASCADE())
-    points= models.ForeignKey(Statistics.points, on_delete=models.CASCADE())
-    class Meta:
-        ordering = ['points']
-    def __str__(self):
-        return self.name
 
 class Contract(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
@@ -55,6 +56,6 @@ class Contract(models.Model):
 class Squad(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-class ScorerTable(models.Model):
+class Scorer_Table(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     goal = models.IntegerField()
