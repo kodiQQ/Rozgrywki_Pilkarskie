ab9adb8401bd2f794c6c53559d392f8287a07f54
Michał Gasiewicz <michal.gasiewicz03@interia.pl>
2024-04-19 16:49:12 +0200

    Login i logout dodany do strony głównej

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
+
+def loginPage(request):
+
+    if request.user.is_authenticated:
+       return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+             user = User.objects.get(username=username)
+        except:
+             messages.error(request, 'Podany użytkownik nie istnieje')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Błędna nazwa użytkownika lub hasło')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 def home(request):
     return render(request,'base/home.html')
